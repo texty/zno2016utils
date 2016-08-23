6361841a5c9e1a63319987d4d761fc3ed6f149da
Vladislav <drimacus182@gmail.com>
2016-08-23 13:21:26 +0300

    city type extraction

--- preprocess.R
+++ preprocess.R
@@ -10,10 +10,14 @@ last_word <- function(TerName) {
   return(res)
 } 
 
+first_word <- function(city) {
+  res <- str_extract(city, '^[^ .]+[ .]')
+  return(res)
+}
+
 #df_last <- df %>% 
 #  mutate(ter_last=last_word(TerName)) %>% 
 #  mutate(area_last=last_word(AreaName))
-
 ########################################
 
 is_city_region <- function(TerName) {
@@ -24,7 +28,7 @@ is_region <- function(AreaName) {
   return (str_detect(AreaName, 'район$'))
 }
 
-city_name <- function(TerName, AreaName) {
+city_name_full <- function(TerName, AreaName) {
   res <- ifelse(is_city_region(TerName), AreaName %>% as.character(), TerName %>% as.character())
   return(res)
 }
@@ -34,13 +38,24 @@ region_name <- function(AreaName) {
   return(res)
 }
 
+city_type <- function(city) {
+  res <- str_extract(city, '^(м|с-ще|смт|с)')
+  return(res)
+}
+
+city_name <- function(city) {
+  res <- city %>% str_replace('^(м.|с-ще |смт |с.)', '') %>% str_trim()
+  return(res)
+}
+
 #todo out to csv
 df0 <- df %>% 
-  mutate(city=city_name(TerName, AreaName)) %>% 
+  mutate(city_full=city_name_full(TerName, AreaName)) %>% 
   mutate(region=region_name(AreaName)) %>%
   mutate(city_region=ifelse(is_city_region(TerName), TerName, NA)) %>% 
-  rename(oblast=Regname) %>% 
-  select(-AreaName, -TerName)
+  mutate(city_type=city_type(city_full)) %>% 
+  mutate(city_name=city_name(city_full)) %>% 
+  rename(oblast=Regname)
 
 df0 %>% write.csv('zno_2016_final.csv', row.names=FALSE) 
   
